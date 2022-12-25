e98e1dca2f7633cfb9215c1deaabc13b691bdc3e
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-25 22:14:39 +0200

    vdjvn

---
+++ src/js/01-gallery.js
@@ -0,0 +1,10 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+// Описаний в документації
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
---
+++ src/js/02-video.js
@@ -0,0 +1,3 @@
+
+import Player from '@vimeo/player';
+ console.log(Player);
