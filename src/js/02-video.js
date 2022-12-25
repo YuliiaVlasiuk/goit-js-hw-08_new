2903ceefa393eb6f726a05fd14cc2533728643e5
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-26 00:41:45 +0200

    all change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,18 @@
 import Player from '@vimeo/player';
- console.log(Player);
 
+ const iframe = document.querySelector('#vimeo-player');
+  const player = new Player(iframe);
+ 
+ 
+ player.on(
+   'timeupdate',
+   function (currentTime) {
+   localStorage.setItem('videoplayer-current-time', currentTime.seconds)}
+ );
+
+
+ player.on('play', function() {
+      if (localStorage.getItem('videoplayer-current-time')){
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}}
+);
